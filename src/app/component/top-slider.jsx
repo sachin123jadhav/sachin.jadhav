94a24e35432100f73a03c481aee08a7deabae7c6
sachin123jadhav <46998326+sachin123jadhav@users.noreply.github.com>
2025-12-07 22:29:02 +0530

    photo changes

--- src/app/component/top-slider.jsx
+++ src/app/component/top-slider.jsx
@@ -63,8 +63,11 @@ function TopSlider() {
               <Image
                 src={heroSachin}
                 alt="Sachin Jadhav"
-                className="mx-auto w-full max-w-[460px] mt-10  rounded-tl-[50%] rounded-br-[50%] shadow-lg  border-l-[5px]
-      border-r-[8px] border-r-orange-500 border-l-red-300"
+                className=" mx-auto w-full max-w-[460px] mt-10 rounded-tl-[50%] rounded-br-[50%] shadow-lg
+
+      border-l-[5px] border-l-red-300
+      border-r-[8px] border-r-orange-500
+      animate-borderColorAnimation "
               />
             </div>
           </div>
--- src/app/tailwind.config.js
+++ src/app/tailwind.config.js
@@ -12,23 +12,34 @@ module.exports = {
           "0%": { transform: "translateX(-50px)", opacity: "0" },
           "100%": { transform: "translateX(0)", opacity: "1" },
         },
+
+        borderColorAnimation: {
+          "0%, 100%": { borderColor: "rgb(47, 180, 174)" },
+          "25%": { borderColor: "rgb(37, 88, 171)" },
+          "50%": { borderColor: "rgb(199, 31, 31)" },
+          "75%": { borderColor: "rgb(255, 205, 0)" },
+        },
       },
+
       animation: {
         "custom-anim-left":
           "custom-anim-left 1.3s forwards cubic-bezier(0.645, 0.045, 0.355, 1) 0.4s",
+
+        borderColorAnimation: "borderColorAnimation 4s linear infinite",
       },
+
       colors: {
         pinkC: {
-          50: "#ffe6f0", // Lightest pink
+          50: "#ffe6f0",
           100: "#ffccd9",
           200: "#ff99b3",
-          300: "#ff6699", // Lighter pink
+          300: "#ff6699",
           400: "#ff3385",
-          500: "#ff4b7e", // Custom Pink (main color)
+          500: "#ff4b7e",
           600: "#e64373",
           700: "#b7365f",
           800: "#9b2d4d",
-          900: "#6e1b33", // Darkest pink
+          900: "#6e1b33",
         },
         redC: {
           50: "#ffebef",
@@ -36,7 +47,7 @@ module.exports = {
           200: "#ff99a2",
           300: "#ff6675",
           400: "#ff3347",
-          500: "#ff4b7e", // Custom Red
+          500: "#ff4b7e",
           600: "#e04371",
           700: "#b7365d",
           800: "#9b2d4b",
@@ -48,7 +59,7 @@ module.exports = {
           200: "#ffdb4d",
           300: "#ffcc00",
           400: "#e6b800",
-          500: "#fcc760", // Custom Yellow
+          500: "#fcc760",
           600: "#e6b44d",
           700: "#cc9f3a",
           800: "#b38a28",
@@ -60,7 +71,7 @@ module.exports = {
           200: "#ff9933",
           300: "#ff751f",
           400: "#ff5a14",
-          500: "#ff7551", // Custom Orange
+          500: "#ff7551",
           600: "#e66a4a",
           700: "#cc5e40",
           800: "#b34c36",
@@ -72,7 +83,7 @@ module.exports = {
           200: "#80c9ff",
           300: "#4db2ff",
           400: "#1a9bff",
-          500: "#00c8d8", // Custom SkyBlue
+          500: "#00c8d8",
           600: "#00a3c1",
           700: "#008da3",
           800: "#007380",
@@ -84,7 +95,7 @@ module.exports = {
           200: "#b466ff",
           300: "#9b29d7",
           400: "#8a00bf",
-          500: "#7709d7", // Custom Purple
+          500: "#7709d7",
           600: "#6600c1",
           700: "#5500a3",
           800: "#440085",
@@ -93,18 +104,17 @@ module.exports = {
       },
     },
   },
+
   plugins: [],
+
   safelist: [
-    'list-none',
-    'space-y-4',
-    'pl-6',
-    'flex',
-    'items-start',
-    'hidden',
-    'bg-pink-500',
-    'border-l-pink-500',
-    'flex',
-    'items-start',
-    // ...all other classes you use dynamically
+    "list-none",
+    "space-y-4",
+    "pl-6",
+    "flex",
+    "items-start",
+    "hidden",
+    "bg-pink-500",
+    "border-l-pink-500",
   ],
 };
