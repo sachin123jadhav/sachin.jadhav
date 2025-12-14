ad5e4b00a49a8fa41f892206899b0ed48c002b67
sachin123jadhav <46998326+sachin123jadhav@users.noreply.github.com>
2025-12-14 23:03:28 +0530

    svg icons

--- src/app/component/projects.jsx
+++ src/app/component/projects.jsx
@@ -23,7 +23,7 @@ export default function PortfolioFilter() {
 
   return (
     <>
-      <section id="projects" className="relative md:py-[60px] py-16 md:pb-12 pb-[100px]"> 
+      <section id="projects" className="relative md:py-24 py-24  "> 
         <div className="container lg mx-auto">
           <div className="flex flex-col justify-between items-center mb-10">
             <h6 className="text-cyan-600 text-base font-medium mb-2 ">
